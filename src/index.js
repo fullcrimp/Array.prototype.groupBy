ac4c1a4558b3b477f762b17cee452eb912afff8d
nik <fullcrimp@protonmail.com>
2017-12-12 18:29:26 +0300

    add fckng awesome solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-module.exports = function groupByFunction(fn) {
-    return function(array) {
+module.exports = (fn = (val => val)) => 
+    (array) => 
+        array.reduce((acc, val) => {
+            let key = fn(val)
+            acc[key] = acc[key] ? [...acc[key], val] : [val]
+            return acc
+        }, {})
 
-        if (!fn) fn = function(val){
-            return val
-        }
-        
-        var result = {}
-
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index]
-            const key = fn(element)
-
-            if(!result[key]) {
-                result[key] = []
-            }
-            result[key].push(element)
-        }
-
-        return result
-    }
-}
