e1a1aa8e0e6e761e901a41d9b8636b4cce727806
fullcrimp <fullcrimp@protonmail.com>
2017-12-12 23:36:33 +0300

    add even more fckng awesome solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-module.exports = (fn = (val => val)) => 
-    (array) => 
-        array.reduce((acc, val) => {
-            let key = fn(val)
-            acc[key] = acc[key] ? [...acc[key], val] : [val]
-            return acc
-        }, {})
+module.exports = (fn = val => val) => array => array.reduce((acc, val) => (acc[fn(val)] = acc[fn(val)] ? [...acc[fn(val)], val] : [val]) ? acc : null, {})
 
