08720bc88cc7d935d570906e73598f5ab470d601
nik <fullcrimp@protonmail.com>
2017-12-12 17:54:29 +0300

    add brute force

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function groupBy(function) {
-    // your solution here
+module.exports = function groupByFunction(fn) {
+    return function(array) {
+
+        if (!fn) fn = function(val){
+            return val
+        }
+        
+        var result = {}
+
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index]
+            const key = fn(element)
+
+            if(!result[key]) {
+                result[key] = []
+            }
+            result[key].push(element)
+        }
+
+        return result
+    }
 }
