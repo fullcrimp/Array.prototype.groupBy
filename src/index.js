725a156644f7bec4890a4a979f92f5e44905a179
Nikolay (Front End Architect MSM-Dawn) <fullcrimp@protonmail.com>
2017-12-13 09:40:24 +0300

    make readable

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-module.exports = (fn = val => val) => array => array.reduce((acc, val) => (acc[fn(val)] = acc[fn(val)] ? [...acc[fn(val)], val] : [val]) ? acc : null, {})
-
+module.exports = (fn = val => val) => 
+    (array, key) => 
+        array.reduce((acc, val) => 
+            (
+                Number.isFinite(key = fn(val)) &&
+                (acc[key] = acc[key] ? [...acc[key], val] : [val])
+            ) ? acc : acc
+        , {})
